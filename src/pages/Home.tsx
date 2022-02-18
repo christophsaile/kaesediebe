9d5c0ab0c8b3341b4ab9a1f937304e5300412ef0
Christoph <christoph.saile@googlemail.com>
2022-02-18 22:01:28 +0100

    feat: add Contentful connection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,32 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useEffect, useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+const contentful = require('contentful');
 
 const Home: React.FC = () => {
+  const [contentfulData, setContentfulData] = useState();
+
+  useEffect(() => {
+    const client = contentful.createClient({
+      space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+      accessToken: process.env.REACT_APP_CONTENTFUL_DELIVERY_API_KEY,
+    });
+
+    client
+      .getEntries({
+        content_type: 'recipe',
+      })
+      .then((entries: any) => {
+        setContentfulData(entries.items);
+      })
+      .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    console.log(contentfulData);
+  }, [contentfulData]);
+
   return (
     <IonPage>
       <IonHeader>
@@ -11,9 +35,9 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <IonHeader collapse='condense'>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size='large'>Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
