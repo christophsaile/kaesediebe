2e7ca02aa2417098866e03d0f7efea18ad3c15bb
Christoph <christoph.saile@googlemail.com>
2022-07-05 19:52:41 +0200

    feat: add linter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("daisyui")],
-	daisyui: {
-		themes: ['light']
-	}
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light'],
+  },
+};
