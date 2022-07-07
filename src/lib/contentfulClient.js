8ab6e937a17daf071f25402340490815467c42cc
Christoph <christoph.saile@googlemail.com>
2022-07-07 19:59:42 +0200

    feat: add prettier

--- src/lib/contentfulClient.js
+++ src/lib/contentfulClient.js
@@ -1,6 +1,6 @@
 import contentful from 'contentful';
 
 export const client = contentful.createClient({
-  space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.CONTENTFUL_DELIVERY_API_KEY,
+	space: import.meta.env.CONTENTFUL_SPACE_ID,
+	accessToken: import.meta.env.CONTENTFUL_DELIVERY_API_KEY,
 });
--- src/types/generated/contentful.d.ts
+++ src/types/generated/contentful.d.ts
@@ -4,68 +4,68 @@ import { Asset, Entry } from 'contentful';
 import { Document } from '@contentful/rich-text-types';
 
 export interface IIngredientFields {
-  /** title */
-  title: string;
+	/** title */
+	title: string;
 }
 
 export interface IIngredient extends Entry<IIngredientFields> {
-  sys: {
-    id: string;
-    type: string;
-    createdAt: string;
-    updatedAt: string;
-    locale: string;
-    contentType: {
-      sys: {
-        id: 'ingredient';
-        linkType: 'ContentType';
-        type: 'Link';
-      };
-    };
-  };
+	sys: {
+		id: string;
+		type: string;
+		createdAt: string;
+		updatedAt: string;
+		locale: string;
+		contentType: {
+			sys: {
+				id: 'ingredient';
+				linkType: 'ContentType';
+				type: 'Link';
+			};
+		};
+	};
 }
 
 export interface IRecipeFields {
-  /** Title */
-  title: string;
+	/** Title */
+	title: string;
 
-  /** Slug */
-  slug: string;
+	/** Slug */
+	slug: string;
 
-  /** Duration */
-  duration: 'Gering' | 'Hoch';
+	/** Duration */
+	duration: 'Gering' | 'Hoch';
 
-  /** Vegetarian */
-  vegetarian: boolean;
+	/** Vegetarian */
+	vegetarian: boolean;
 
-  /** Category */
-  category: 'Reis' | 'Nudeln' | 'Kartoffeln' | 'Sonstiges';
+	/** Category */
+	category: 'Reis' | 'Nudeln' | 'Kartoffeln' | 'Sonstiges';
 
-  /** Ingredients */
-  ingredients: Record<string, any>;
+	/** Ingredients */
+	ingredients: Record<string, any>;
 
-  /** Description */
-  description?: Document | undefined;
+	/** Description */
+	description?: Document | undefined;
 
-  /** Image */
-  image?: Asset | undefined;
+	/** Image */
+	image?: Asset | undefined;
 }
 
 export interface IRecipe extends Entry<IRecipeFields> {
-  sys: {
-    id: string;
-    type: string;
-    createdAt: string;
-    updatedAt: string;
-    locale: string;
-    contentType: {
-      sys: {
-        id: 'recipe';
-        linkType: 'ContentType';
-        type: 'Link';
-      };
-    };
-  };
+	sys: {
+		id: string;
+		type: string;
+		createdAt: string;
+		updatedAt: string;
+		locale: string;
+		contentType: {
+			sys: {
+				id: 'recipe';
+				linkType: 'ContentType';
+				type: 'Link';
+			};
+		};
+	};
 }
 
 export type CONTENT_TYPE = 'ingredient' | 'recipe';
