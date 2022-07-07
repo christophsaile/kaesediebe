f7b54ccc9bb48b59b706920f1645cf87de6ea2af
Christoph <christoph.saile@googlemail.com>
2022-07-07 21:54:55 +0200

    feat: add card styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: ['light'],
-  },
+	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				kaesediebe: {
+					primary: '#fef08a',
+					secondary: '#d9f99d',
+					accent: '#37CDBE',
+					neutral: '#3D4451',
+					'base-100': '#FFFFFF',
+					info: '#3ABFF8',
+					success: '#36D399',
+					warning: '#FBBD23',
+					error: '#F87272',
+				},
+			},
+		],
+	},
 };
