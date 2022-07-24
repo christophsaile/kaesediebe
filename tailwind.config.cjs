e6e539a1fef01235600dea1952ef9bdf53c2c267
Christoph <christoph.saile@googlemail.com>
2022-07-24 18:17:05 +0200

    feat: add share function

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -25,7 +25,7 @@ module.exports = {
 					neutral: '#e5e7eb',
 					'base-100': '#FFFFFF',
 					info: '#3ABFF8',
-					success: '#36D399',
+					success: '#d9f99d',
 					warning: '#FBBD23',
 					error: '#F87272',
 				},
