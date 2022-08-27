7efb368ab53d1aaeb88243a4c57368a828ef6115
Christoph <christoph.saile@googlemail.com>
2022-08-27 16:34:07 +0200

    feat: add page config

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const SITE = {
+	title: 'Käsediebe',
+	description: 'Käsediebe ist ein Foodblog in dem Christoph und Pauline ihre Rezepte vorstellen.',
+	url: 'https://kaese-diebe.netlify.app',
+}
+
+export const PAGE_SIZE = 10
