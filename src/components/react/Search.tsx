7e8bbc77e6467c5afb3fb257ea0e74d11387bcdb
Christoph <christoph.saile@googlemail.com>
2022-08-30 22:00:14 +0200

    feat: add search

--- src/components/react/Search.tsx
+++ src/components/react/Search.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource react */
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-hooks-web';
+import { InstantSearch, SearchBox } from 'react-instantsearch-hooks-web';
+import CustomHits from './CustomHits';
 import 'instantsearch.css/themes/satellite.css';
 
 type ISearch = {
@@ -16,14 +17,37 @@ const Search = ({ env }: ISearch) => {
 
 	return (
 		<InstantSearch indexName={env.index} searchClient={searchClient}>
-			<SearchBox></SearchBox>
-			<Hits></Hits>
+			<CustomHits />
+			<SearchBox
+				autoFocus
+				placeholder="Suche nach Rezepten ..."
+				classNames={{
+					root: 'fixed bottom-20 left-0 right-0 mx-4',
+					form: '!h-12 !bg-transparent before:!hidden',
+					input:
+						'!rounded-xl !bg-base-100 focus:!border-current !border-neutral-focus !shadow-lg !text-current !caret-current placeholder:!text-neutral-focus',
+					resetIcon: '!fill-current',
+					submit: 'absolute !block !left-4 !top-0 !bottom-0',
+				}}
+				submitIconComponent={() => (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-4 w-4"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="2"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+						></path>
+					</svg>
+				)}
+			/>
 		</InstantSearch>
 	);
 };
-
-function Hit({ hit }) {
-	return JSON.stringify(hit);
-}
 
 export default Search;
