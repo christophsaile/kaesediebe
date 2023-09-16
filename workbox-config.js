a0b7ac05efc0c01514a825c4e2c90e96cacff12d
Christoph <christoph.saile@googlemail.com>
2023-09-16 21:33:27 +0200

    chore: update workbox

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,24 @@
+function generateCacheName() {
+	const timestamp = Date.now();
+	return `my-cache-${timestamp}`;
+}
+
 module.exports = {
 	globDirectory: 'dist/',
 	globPatterns: ['**/*.{html,css,png,ico,js,json,webp}'],
 	swDest: 'dist/sw.js',
 	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+	runtimeCaching: [
+		{
+			urlPattern: /\.(?:png|jpg|jpeg|svg|webp|html|css|js|json)$/,
+			handler: 'CacheFirst',
+			options: {
+				cacheName: generateCacheName(),
+				expiration: {
+					maxEntries: 10,
+					maxAgeSeconds: 24 * 60 * 60,
+				},
+			},
+		},
+	],
 };
