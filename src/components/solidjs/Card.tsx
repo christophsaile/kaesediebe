d0300fb53ec8145af6608fa465aa3ed49cd0ab27
Christoph <christoph.saile@googlemail.com>
2024-10-04 13:01:34 +0200

    feat: improve a11y

--- src/components/solidjs/Card.tsx
+++ src/components/solidjs/Card.tsx
@@ -12,45 +12,45 @@ const Card: ParentComponent<ICard> = (props) => {
 	const { data, cardNumber } = props;
 
 	return (
-		<a href={`recipe/${data.slug}/`} class="rounded-2xl">
-			<article class="card card-side bg-base-100 shadow-xl">
-				<figure class="w-2/5 flex-none bg-neutral">
-					{data.imgUrl ? (
-						<img
-							{...(cardNumber > 5 ? { loading: 'lazy' } : {})}
-							width="150"
-							height="150"
-							class="rounded-l-2xl"
-							src={data.imgUrl + '?fm=webp'}
-							alt=""
-						/>
-					) : (
-						<img
-							width="150"
-							height="150"
-							src={placeholder.src}
-							alt=""
-							class="rounded-l-2xl"
-						/>
-					)}
-				</figure>
-				<div class="card-body p-3">
-					<div class="card-title flex gap-1 flex-wrap [hyphens:auto] [-webkit-hyphens:auto]">
+		<article class="card card-side bg-base-100 shadow-xl relative">
+			<figure class="w-2/5 flex-none bg-neutral">
+				{data.imgUrl ? (
+					<img
+						{...(cardNumber > 5 ? { loading: 'lazy' } : {})}
+						width="150"
+						height="150"
+						class="rounded-l-2xl"
+						src={data.imgUrl + '?fm=webp'}
+						alt=""
+					/>
+				) : (
+					<img
+						width="150"
+						height="150"
+						src={placeholder.src}
+						alt=""
+						class="rounded-l-2xl"
+					/>
+				)}
+			</figure>
+			<div class="card-body p-3">
+				<div class="card-title flex gap-1 flex-wrap [hyphens:auto] [-webkit-hyphens:auto]">
+					<a href={`recipe/${data.slug}/`} class="rounded-2xl a11y-link">
 						<h2>{data.title}</h2>
-						{data.vegetarian && (
-							<span
-								class="badge badge-secondary"
-								aria-label="Vegetarisches Gericht"
-							>
-								Veggi
-							</span>
-						)}
-					</div>
-					<Badges duration={data.duration} category={data.category} />
-					{props.children}
+					</a>
+					{data.vegetarian && (
+						<span
+							class="badge badge-secondary"
+							aria-label="Vegetarisches Gericht"
+						>
+							Veggi
+						</span>
+					)}
 				</div>
-			</article>
-		</a>
+				<Badges duration={data.duration} category={data.category} />
+				{props.children}
+			</div>
+		</article>
 	);
 };
 
